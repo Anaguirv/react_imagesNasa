e0606313fe62749e8dd9949ce5acd2fbcd52fd1f
Arnoldo Aguirre <arnoldo.aguirre@inacapmail.cl>
2024-07-15 21:38:15 -0400

    Se elimino github page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Anaguirv.github.io/react_imagesNasa/",
 })
