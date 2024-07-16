435d457bd65cb69600132096edc1aa5958ff5033
Arnoldo Aguirre <arnoldo.aguirre@inacapmail.cl>
2024-07-15 21:42:35 -0400

    crear github page

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://Anaguirv.github.io/react_imagesNasa/",
   plugins: [react()],
 })
